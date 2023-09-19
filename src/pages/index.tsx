d82144381a747b84740b2cd79b3cd5daa3bf76e5
sopoour <sophia.auer@googlemail.com>
2023-09-19 19:28:49 +0200

    change card effect on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,11 @@ import MaxWidthContainer from '../components/MaxWidthContainer';
 import SignupForm from '../components/SignupForm';
 import { fetcher } from '../hooks/fetch/useFetch';
 import Sidebar from '../components/Sidebar/Sidebar';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import MarkdownConfig from '../components/MarkdownConfig/MarkdownConfig';
+import { useMedia } from '../hooks/useMedia';
+import { Breakpoints } from '../styles/media';
 
 const Root = styled.div`
   display: flex;
@@ -25,17 +27,18 @@ const Root = styled.div`
 
 const CardWrapper = styled.div`
   display: flex;
-  flex-direction: column;
   height: 100%;
-
-  ${Card}:last-of-type {
-    margin-bottom: unset;
-  }
+  width: 100%;
+  z-index: 0;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  scroll-snap-type: x mandatory;
+  scroll-behavior: smooth;
+  -webkit-overflow-scrolling: touch;
 
   ${({ theme }) => theme.media('sm')`
-    flex-direction: row;
     ${Card}:not(:first-of-type):hover {
-      transform: translate(160px, -25px);
+      transform: translateX(160px);
     }
 
     ${Card}:hover ~ ${Card} {
@@ -58,13 +61,16 @@ const DetailContainer = styled.div`
 `;
 
 const Page: NextPage = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const [open, setOpen] = useState<boolean>(false);
+  const [isVisibleCard, setIsVisibleCard] = useState<number>(-1);
   const [linkedPost, setLinkedPost] = useState<NewsletterPost>();
   const { data, isLoading } = useSWR<NewsletterPost[] | null>('/api/posts', fetcher);
   const router = useRouter();
   const { slug } = router.query;
   const { data: descriptionData } = useSWR<GeneralContent | null>('/api/general', fetcher);
   const description = descriptionData?.pageDescription;
+  const isDesktop = useMedia(Breakpoints.sm);
 
   useEffect(() => {
     if (
@@ -80,14 +86,52 @@ const Page: NextPage = () => {
     }
   }, [slug, isLoading, data]);
 
+  const handleScroll = useCallback(() => {
+    const container = containerRef.current;
+    const scrollPosition = container?.scrollLeft;
+
+    if (container?.children) {
+      // Your logic to determine which card is in the viewport
+      const cards = Array.from(container?.children); // Get all child elements
+      cards.forEach((card, index) => {
+        if (card instanceof HTMLElement) {
+          const cardLeft = card.offsetLeft;
+          const cardRight = cardLeft + card.clientWidth;
+
+          if (scrollPosition && scrollPosition >= cardLeft - 50 && scrollPosition < cardRight) {
+            setIsVisibleCard(index);
+          }
+        }
+      });
+    }
+  }, [isVisibleCard]);
+
+  useEffect(() => {
+    // Attach the scroll event listener to the container
+    const containerElement = containerRef.current;
+    containerElement?.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      containerElement?.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <Root>
       <TopWrapper>
         {description && <Typography textalign="center">{description}</Typography>}
         <SignupForm />
       </TopWrapper>
-      <CardWrapper>
-        {data?.map((post, index) => <NewsletterCard post={post} stack={index} key={post.slug} />)}
+      <CardWrapper ref={containerRef}>
+        {data?.map((post, index) => (
+          <NewsletterCard
+            post={post}
+            stack={index}
+            key={post.slug}
+            isActiveCard={isVisibleCard === index && !isDesktop}
+          />
+        ))}
       </CardWrapper>
       <Sidebar
         side="left"
